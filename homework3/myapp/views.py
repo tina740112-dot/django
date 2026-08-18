4922b3dad6b3ea89670a9aa936c67cf30bab5e54
unknown <tina740112@gmail.com>
2026-08-18 17:26:37 +0800

    update homework3

--- homework3/myapp/views.py
+++ homework3/myapp/views.py
@@ -1,27 +1,25 @@
 from django.shortcuts import render
 
-from django.shortcuts import render
-
 def homework3(request):
     if request.method == 'POST':
-        name = request.POST.get('name', '')
+        name = request.POST.get('username', '')
         gender = request.POST.get('gender', '')
         hobbies = request.POST.getlist('hobby')
-        
-        # 判定性別稱謂
+
+        # 判斷性別稱謂
         if gender == '男':
-            title = '先生'
+            title = '先生您好！'
         elif gender == '女':
-            title = '您好'
+            title = '小姐您好！'
         else:
-            title = '你好'
+            title = '您好！'
 
         context = {
             'name': name,
             'title': title,
             'hobbies': hobbies,
         }
-        return render(request, 'homework3.html', context)
+        # 這裡改為你截圖中的檔名 homework3_result.html
+        return render(request, 'homework3_result.html', context)
 
-    # GET 請求 (第一次載入頁面)
     return render(request, 'homework3.html')
